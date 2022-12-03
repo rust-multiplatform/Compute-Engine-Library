0e736032296602d922219b13111fe7dc2fdd7d69
Lukas Weber <me@sakul6499.de>
2022-12-03 21:03:27 +0100

    Removed old base-engine, added new compute engine

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,150 @@
 #![deny(clippy::all)]
 
-mod logger;
-pub use logger::*;
+use std::{sync::Arc, time::Instant};
 
-mod logical_device;
-pub use logical_device::*;
+use base_engine::{log_init, BaseEngine, LogicalDevice};
+use vulkano::{
+    command_buffer::{PrimaryAutoCommandBuffer, PrimaryCommandBufferAbstract},
+    device::{physical::PhysicalDevice, Device, DeviceCreateInfo, Queue, QueueCreateInfo},
+    instance::{Instance, InstanceCreateInfo},
+    sync::GpuFuture,
+    VulkanLibrary,
+};
 
-use std::sync::Arc;
+pub struct ComputeEngine {
+    instance: Arc<Instance>,
+    logical_device: Arc<LogicalDevice>,
+}
 
-use vulkano::{command_buffer::PrimaryAutoCommandBuffer, instance::Instance};
+impl ComputeEngine {
+    /// Creates a `ComputeEngine` instance and initializes everything needed for computing tasks.
+    pub fn new() -> Self {
+        log_init();
+        log::debug!("ComputeEngine::startup");
 
-use crate::LogicalDevice;
+        let instance = Self::create_instance();
+        Self::print_api_information(instance.clone(), log::Level::Debug);
 
-pub trait BaseEngine {
-    /// Kills the engine as a safety measure in case other things like winit aren't killing the Engine part.
-    fn kill(&self) {
-        log::debug!("::: KILLING MAIN PROCESS :::");
-        std::process::exit(std::process::id().try_into().unwrap());
+        let physical_device = Self::get_physical_device(instance.clone());
+        let queue_family_index = Self::find_best_suited_queue_family(physical_device.clone());
+        let logical_device = Self::create_logical_device(physical_device, queue_family_index);
+
+        logical_device.print_interesting_information(log::Level::Debug);
+
+        Self {
+            instance,
+            logical_device,
+        }
     }
 
-    /// Prints out information about the Vulkan API.
-    fn print_api_information(instance: Arc<Instance>, log_level: log::Level) {
-        log::log!(log_level, "API Version: {}", instance.api_version());
-        log::log!(log_level, "Max API Version: {}", instance.max_api_version());
+    /// Creates a Vulkan(o) instance.
+    fn create_instance() -> Arc<Instance> {
+        log::debug!("ComputeEngine::create_instance");
+
+        let library = VulkanLibrary::new()
+            .expect("failed to load Vulkan library. Make sure you have the Vulkan SDK installed.");
+
+        Instance::new(library, InstanceCreateInfo::application_from_cargo_toml())
+            .expect("failed to create Vulkan instance")
     }
 
-    /// Computes a given operation on the GPU
-    fn compute(&self, operation: &dyn (Fn(&Self) -> PrimaryAutoCommandBuffer));
+    /// Queries all `PhysicalDevice`'s and returns the best match.
+    fn get_physical_device(instance: Arc<Instance>) -> Arc<PhysicalDevice> {
+        log::debug!("ComputeEngine::get_physical_device");
 
-    /// Returns the instance of the Vulkan API.
-    fn get_instance(&self) -> Arc<Instance>;
+        instance
+            .enumerate_physical_devices()
+            .expect("failed enumerating physical devices")
+            .filter(|physical_device: &Arc<PhysicalDevice>| {
+                physical_device
+                    .queue_family_properties()
+                    .iter()
+                    .any(|queue_family| queue_family.queue_flags.compute)
+            })
+            .min_by_key(|physical_device: &Arc<PhysicalDevice>| {
+                match physical_device.properties().device_type {
+                    vulkano::device::physical::PhysicalDeviceType::DiscreteGpu => 0,
+                    vulkano::device::physical::PhysicalDeviceType::IntegratedGpu => 1,
+                    vulkano::device::physical::PhysicalDeviceType::VirtualGpu => 2,
+                    vulkano::device::physical::PhysicalDeviceType::Cpu => 3,
+                    vulkano::device::physical::PhysicalDeviceType::Other => 4,
+                    _ => 5,
+                }
+            })
+            .expect("no physical device found or available")
+    }
 
-    /// Returns the logical device.
-    fn get_logical_device(&self) -> Arc<LogicalDevice>;
+    /// Finds the best suited `QueueFamily` of a given device or none if no suitable family was found.
+    fn find_best_suited_queue_family(physical_device: Arc<PhysicalDevice>) -> u32 {
+        log::debug!("ComputeEngine::find_best_suited_queue_family");
+
+        let queue_family_index = physical_device
+            .queue_family_properties()
+            .iter()
+            .enumerate()
+            .position(|(_, q)| q.queue_flags.compute) // Find a compute capable queue family
+            .expect("no suitable queue family found") as u32;
+
+        log::debug!("Queue family index: {}", queue_family_index);
+        queue_family_index
+    }
+
+    /// Creates a `LogicalDevice` given a `PhysicalDevice` and a `QueueFamilyIndex`.
+    fn create_logical_device(
+        physical_device: Arc<PhysicalDevice>,
+        queue_family_index: u32,
+    ) -> Arc<LogicalDevice> {
+        log::debug!("ComputeEngine::create_logical_device");
+
+        let (device, raw_queues) = Device::new(
+            physical_device,
+            DeviceCreateInfo {
+                queue_create_infos: vec![QueueCreateInfo {
+                    queue_family_index,
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        )
+        .expect("failed to create logical device");
+
+        let queues: Vec<Arc<Queue>> = raw_queues.collect();
+
+        let logical_device = LogicalDevice::new(device, queue_family_index, queues);
+        Arc::new(logical_device)
+    }
+}
+
+impl BaseEngine for ComputeEngine {
+    fn compute(&self, operation: &dyn (Fn(&Self) -> PrimaryAutoCommandBuffer)) {
+        let command_buffer = operation(&self);
+
+        #[cfg(debug_assertions)]
+        let start_fence = Instant::now();
+
+        command_buffer
+            .execute(self.get_logical_device().get_first_queue().clone())
+            .unwrap()
+            .then_signal_fence_and_flush()
+            .unwrap()
+            .wait(None)
+            .unwrap();
+
+        #[cfg(debug_assertions)]
+        {
+            let end_fence = Instant::now();
+            log::debug!(
+                "Compute operation took: {}ms",
+                end_fence.duration_since(start_fence).as_millis()
+            );
+        }
+    }
+
+    fn get_instance(&self) -> Arc<Instance> {
+        self.instance.clone()
+    }
+
+    fn get_logical_device(&self) -> Arc<LogicalDevice> {
+        self.logical_device.clone()
+    }
 }
